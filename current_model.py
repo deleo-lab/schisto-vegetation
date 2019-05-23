f6a7dbfa959f6a1de2a56dd2a947d97dd19090d2
John Bauer <horatio@gmail.com>
2019-05-23 09:26:16 -0700

    Initial model copied from the python notebook

---
+++ current_model.py
@@ -0,0 +1,216 @@
+import numpy as np 
+import os
+import random
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+
+import tensorflow as tf
+
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+import tensorflow.keras.backend as backend
+
+from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+PATCH_SZ = 256   # should divide by 16
+BATCH_SIZE = 8
+TRAIN_SZ = 50  # train size
+VAL_SZ = 15   # validation size
+N_EPOCHS = 500  #150
+
+HOME = "."
+path_TIF = '%s/training_set/8_bands_set3/' % HOME
+water_mask = '%s/training_set/water_mask_set3/' % HOME
+land_mask = '%s/training_set/land_mask_set3/' % HOME
+emergent_mask = '%s/training_set/emergent_mask_set3/' % HOME
+ceratophyllum_mask = '%s/training_set/cera_mask_set3/' % HOME
+
+num_classes = 3
+num_training_images = 50
+X_DICT_TRAIN = dict()
+Y_DICT_TRAIN = dict()
+X_DICT_VALIDATION = dict()
+Y_DICT_VALIDATION = dict()
+
+STARTING_LR = 4e-5
+
+def get_rand_patch(img, mask, sz):
+    """
+    :param img: ndarray with shape (x_sz, y_sz, num_channels)
+    :param mask: binary ndarray with shape (x_sz, y_sz, num_classes)
+    :param sz: size of random patch
+    :return: patch with shape (sz, sz, num_channels)
+    """
+    assert len(img.shape) == 3
+    assert img.shape[0] >= sz
+    assert img.shape[1] >= sz
+    assert img.shape[0:2] == mask.shape[0:2]
+    xc = random.randint(0, img.shape[0] - sz)
+    yc = random.randint(0, img.shape[1] - sz)
+
+    patch_img = img[xc:(xc + sz), yc:(yc + sz)]
+    patch_mask = mask[xc:(xc + sz), yc:(yc + sz)]
+    
+    #adjusted for masks of size (512,512), no third dimension.
+    
+    # Apply some random transformations
+    random_transformation = np.random.randint(1,8)
+    if random_transformation == 1:
+        # reverse first dimension
+        patch_img = patch_img[::-1,:,:]
+        patch_mask = patch_mask[::-1,:,:] #[::-1,:,:]
+    elif random_transformation == 2:
+        # reverse second dimension
+        patch_img = patch_img[:,::-1,:]
+        patch_mask = patch_mask[:,::-1,:] #[:,::-1,:]
+    elif random_transformation == 3:
+        # transpose(interchange) first and second dimensions
+        patch_img = patch_img.transpose([1,0,2])
+        patch_mask = patch_mask.transpose([1,0,2])
+    elif random_transformation == 4:
+        patch_img = np.rot90(patch_img, 1)
+        patch_mask = np.rot90(patch_mask, 1)
+    elif random_transformation == 5:
+        patch_img = np.rot90(patch_img, 2)
+        patch_mask = np.rot90(patch_mask, 2)
+    elif random_transformation == 6:
+        patch_img = np.rot90(patch_img, 3)
+        patch_mask = np.rot90(patch_mask, 3)
+    else:
+        pass
+
+    return patch_img, patch_mask
+
+
+def get_patches(x_dict, y_dict, n_patches, sz):
+    x = list()
+    y = list()
+    total_patches = 0
+    while total_patches < n_patches:
+        img_id = random.sample(x_dict.keys(), 1)[0]
+        img = x_dict[img_id]
+        mask = y_dict[img_id]
+        img_patch, mask_patch = get_rand_patch(img, mask, sz)
+        x.append(img_patch)
+        y.append(mask_patch)
+        total_patches += 1
+    #print('Generated {} patches'.format(total_patches))
+    return np.array(x), np.array(y)
+
+def unet(learning_rate,classes, pretrained_weights = None, input_size = (256,256,8)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(32, (3, 3), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(32, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    
+    conv2 = Conv2D(64, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(64, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    
+    conv3 = Conv2D(128, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(128, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    
+    conv4 = Conv2D(256, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(256, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.1)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(512, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(512, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.1)(conv5)
+
+    up6 = Conv2D(256, (2, 2), activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis=3)
+    conv6 = Conv2D(256, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(256, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(128, (2, 2),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(128, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(128, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(64, (2, 2),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(64, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(64, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(32, (2, 2),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9],  axis = 3)
+    conv9 = Conv2D(32, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(32, (3, 3),activation = 'elu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(classes,1, activation = 'sigmoid')(conv9)
+
+    # CHANGE: typo in Model ffs
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+    model.compile(optimizer = Adam(lr = learning_rate), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+def read_images():
+    for i in range (0,num_training_images):
+        # TODO: use the directory listing directly rather than
+        # expecting hardcoded names
+        image_filename = path_TIF + '%d.TIF' % i
+        img_m = io.imread(image_filename)
+        img_m = img_m*16   # TODO: convert 11 bit numbers to 16 bit?
+        
+        #create 3d mask where each channel is the mask for a specific class
+        mask = np.zeros((512,512,num_classes))
+        
+        mask_water = io.imread(water_mask + '%d.png'%i)
+        mask_land = io.imread(land_mask + '%d.png'%i)
+        mask_emerg = io.imread(emergent_mask +'%d.png'%i)
+        mask_cera = io.imread(ceratophyllum_mask + '%d.png'%i)
+        
+        mask[:,:,2] = mask_water[:,:]
+        mask[:,:,1] = mask_land[:,:]
+        mask[:,:,0] = mask_emerg[:,:]
+        mask[:,:,0] += mask_cera[:,:]
+        
+        mask = mask/255        
+
+        # CHANGE: use 80% of images for train, 20% for validation
+        if i < num_training_images * 0.8:
+            X_DICT_TRAIN[i] = img_m
+            Y_DICT_TRAIN[i] = mask
+        else:
+            X_DICT_VALIDATION[i] = img_m
+            Y_DICT_VALIDATION[i] = mask
+
+        print(image_filename + ' read')
+
+def train(model):
+    model_name_generated = 'unet_3cW_0.hdf5'
+    for i in range (0,N_EPOCHS):        
+        model_checkpoint = ModelCheckpoint(model_name_generated, monitor='val_loss', save_best_only=True)
+        #csv_logger = CSVLogger('log_unet.csv', append=True, separator=';')
+        #tensorboard = TensorBoard(log_dir='./tensorboard_unet/', write_graph=True, write_images=True)
+
+        x_train, y_train = get_patches(X_DICT_TRAIN, Y_DICT_TRAIN, n_patches=TRAIN_SZ, sz=PATCH_SZ) 
+        x_val, y_val = get_patches(X_DICT_VALIDATION, Y_DICT_VALIDATION, n_patches=VAL_SZ, sz=PATCH_SZ)
+        #model_checkpoint = ModelCheckpoint(model_name_generated, monitor='val_loss', save_best_only=True) 
+        model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=1,
+                  verbose=2, #shuffle=True,
+                  #callbacks=[model_checkpoint, csv_logger, tensorboard],
+                  callbacks=[model_checkpoint],
+                  validation_data=(x_val, y_val),class_weight=[.8,.1,.3])
+        del x_train, y_train
+        print ("Finished epoch %d" % i)
+    
+        model.save_weights(model_name_generated)
+    
+
+read_images()
+model = unet(STARTING_LR, num_classes)
+train(model)
