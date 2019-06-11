497a8f65ec87a8b1ceb230b8bb122b01194ed63b
John Bauer <horatio@gmail.com>
2019-06-10 17:11:51 -0700

    Add an argparser to the straight_rectangle script

--- straighten_rectangle.py
+++ straighten_rectangle.py
@@ -1,3 +1,4 @@
+import argparse
 import re
 import sys
 
@@ -11,18 +12,33 @@ def find_edges(raw):
     return np.asarray([[0, 0], [shape[0], 0],
                        [shape[0], shape[1]], [0, shape[1]]])
 
-if __name__ == '__main__':
-    base_tif = sys.argv[1]
+def parse_args():
+    parser = argparse.ArgumentParser(description='Transform a training/test example.')
+    parser.add_argument('--base_file', default=None,
+                        help='Which file to process')
+    parser.add_argument('--trapezoid', default=None,
+                        help='Trapezoid to preserve from original file')
+
+    args = parser.parse_args()
+    return args
+
+def parse_trapezoid(trapezoid):
     # trapezoid will be stretched in the order UL, UR, LR, LL
-    trapezoid = sys.argv[2]
     pattern = re.compile("^[(), 0-9]+$")
     if not pattern.match(trapezoid):
         raise RuntimeError("Illegal characters in the trapezoid description")
     original = eval(trapezoid)
     original = np.asarray(original)
+    return original
 
-    raw = skimage.io.imread(base_tif)
 
+if __name__ == '__main__':
+    args = parse_args()
+
+    base_file = args.base_file
+    raw = skimage.io.imread(base_file)
+
+    original = parse_trapezoid(args.trapezoid)
     desired = find_edges(raw)
 
     transform = ProjectiveTransform()
