af3612be5401b8c189e628751030db74ee625e07
John Bauer <horatio@gmail.com>
2019-06-11 14:13:20 -0700

    Do a whole directory at once

--- straighten_rectangle.py
+++ straighten_rectangle.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import os
 import re
 import sys
@@ -23,6 +24,16 @@ def parse_args():
     parser.add_argument('--output_suffix', default='_trans',
                         help='Suffix for outputting the transformed file(s)')
     
+    parser.add_argument('--transform_all', dest='transform_all',
+                        default=False, action='store_true',
+                        help='Transform all of 8 channel, cera, etc')
+
+    parser.add_argument('--overwrite', dest='overwrite',
+                        default=False, action='store_true',
+                        help='Force overwrite of existing files... be careful')
+
+    parser.add_argument('--subdirs', default='8_bands,cera_mask,emergent_mask,land_mask,RGB,water_mask')
+
     args = parser.parse_args()
     return args
 
@@ -39,7 +50,8 @@ def parse_trapezoid(trapezoid):
 if __name__ == '__main__':
     args = parse_args()
 
-    base_file = args.base_file
+    base_file = os.path.normpath(args.base_file)
+    raw = {}
     raw = skimage.io.imread(base_file)
 
     original = parse_trapezoid(args.trapezoid)
@@ -48,21 +60,52 @@ if __name__ == '__main__':
     transform = ProjectiveTransform()
     transform.estimate(desired, original)
 
-    base_files = [base_file]
+    if args.transform_all:
+        # we put base_file first so that we can save a read later
+        base_files = [base_file]
+        parent_dir = os.path.split(os.path.split(base_file)[0])[0]
+        base_name = os.path.splitext(os.path.split(base_file)[1])[0]
+        print(parent_dir)
+        subdirs = args.subdirs.split(",")
+        for subdir in subdirs:
+            path = os.path.join(parent_dir, subdir, base_name)
+            images = glob.glob(path + ".*")
+            if len(images) > 1:
+                raise RuntimeError("Found multiple files: %s" % str(images))
+            if len(images) == 0:
+                print("Warning: found no file like %s" % path)
+            next_file = os.path.normpath(images[0])
+            if next_file != base_file:
+                base_files.append(next_file)
+    else:
+        base_files = [base_file]
 
     files = []
     for infile in base_files:
         base, ext = os.path.splitext(infile)
         outfile = base + args.output_suffix + ext
-        if os.path.exists(outfile):
+        if os.path.exists(outfile) and not args.overwrite:
             raise RuntimeError("Cowardly refusing to overwrite %s" % outfile)
         files.append((infile, outfile))
-
+        
     for infile, outfile in files:
+        print("Transforming %s into %s" % (infile, outfile))
+        if infile != base_file:
+            raw = skimage.io.imread(infile)
         t_image = warp(raw, transform)
+        # TODO: saving images causes various errors, such as
+        # masks:
+        #   Lossy conversion from float64 to uint8. Range [0, 1].
+        # 8 band:
+        #   ~ is a low contrast image
+        skimage.io.imsave(outfile, t_image)
 
-        rgb = np.array(t_image * 255, dtype=np.int8)
-        im = Image.fromarray(rgb, "RGB")
-        im.show()
-        im.save(outfile)
+        # TODO: show the image
+        #if len(raw.shape) == 2 or raw.shape[2] == 3:
+        #    skimage.io.imshow(t_image)
+
+        #rgb = np.array(t_image * 255, dtype=np.int8)
+        #im = Image.fromarray(rgb, "RGB")
+        #im.show()
+        #im.save(outfile)
         
