9d2fdd765f0d8320bbfe7cf81fd2375ee66385d9
John Bauer <horatio@gmail.com>
2019-06-11 19:22:34 -0700

    Refactor, add some doc

--- straighten_rectangle.py
+++ straighten_rectangle.py
@@ -39,6 +39,10 @@ def parse_args():
     return args
 
 def parse_trapezoid(trapezoid):
+    """
+    Parse text that looks like this:
+      "((64, 0), (511, 0), (511, 511), (60, 511))"
+    """
     # trapezoid will be stretched in the order UL, UR, LR, LL
     pattern = re.compile("^[(), 0-9]+$")
     if not pattern.match(trapezoid):
@@ -48,6 +52,9 @@ def parse_trapezoid(trapezoid):
     return original
 
 def check_mask(mask):
+    """
+    Verify that the mask is either black or white
+    """
     values = {}
     for i in range(mask.shape[0]):
         for j in range(mask.shape[1]):
@@ -56,6 +63,29 @@ def check_mask(mask):
         if k != 0 and k != 255:
             raise RuntimeError("Mask was not entirely 0 or 255")
 
+def get_sibling_files(base_file, subdirs):
+    """
+    Look for files with similar names to base_file in parallel dirs
+    """
+    # we put base_file first so that we can save a read later
+    base_files = [base_file]
+    parent_dir = os.path.split(os.path.split(base_file)[0])[0]
+    base_name = os.path.splitext(os.path.split(base_file)[1])[0]
+    print("Processing all files in directory %s with base_name %s" %
+          (parent_dir, base_name))
+    subdirs = subdirs.split(",")
+    for subdir in subdirs:
+        path = os.path.join(parent_dir, subdir, base_name)
+        images = glob.glob(path + ".*")
+        if len(images) > 1:
+            raise RuntimeError("Found multiple files: %s" % str(images))
+        if len(images) == 0:
+            print("Warning: found no file like %s" % path)
+        next_file = os.path.normpath(images[0])
+        if next_file != base_file:
+            base_files.append(next_file)
+    return base_files
+        
 if __name__ == '__main__':
     args = parse_args()
 
@@ -70,22 +100,7 @@ if __name__ == '__main__':
     transform.estimate(desired, original)
 
     if args.transform_all:
-        # we put base_file first so that we can save a read later
-        base_files = [base_file]
-        parent_dir = os.path.split(os.path.split(base_file)[0])[0]
-        base_name = os.path.splitext(os.path.split(base_file)[1])[0]
-        print(parent_dir)
-        subdirs = args.subdirs.split(",")
-        for subdir in subdirs:
-            path = os.path.join(parent_dir, subdir, base_name)
-            images = glob.glob(path + ".*")
-            if len(images) > 1:
-                raise RuntimeError("Found multiple files: %s" % str(images))
-            if len(images) == 0:
-                print("Warning: found no file like %s" % path)
-            next_file = os.path.normpath(images[0])
-            if next_file != base_file:
-                base_files.append(next_file)
+        base_files = get_sibling_files(base_file, args.subdirs)
     else:
         base_files = [base_file]
 
